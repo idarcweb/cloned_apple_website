e37924041a25ae8bbfed0ec92ed3733a9a9f209c
idarcweb <idarcqforte@gmail.com>
2024-08-03 03:56:08 +0100

    apple_clone

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import {
+  Navbar,
+  Highlights,
+  Hero,
+  Model,
+  Features,
+  HowItWorks,
+  Footer,
+} from "./components";
+
+const App = () => {
+  return (
+    <main className="bg-black overflow-x-hidden">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Hero } from "./Hero";
+export { default as Navbar } from "./Navbar";
+export { default as Highlights } from "./Highlights";
+export { default as Model } from "./Model";
+export { default as Features } from "./Features";
+export { default as HowItWorks } from "./HowItWorks";
+export { default as Footer } from "./Footer";
